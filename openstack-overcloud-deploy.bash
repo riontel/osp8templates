67ceb15c13a212f8438d0c2472a6b9f4decd7add
R I <riontel@gmail.com>
2016-05-26 15:21:07 -0700

    file upload

---
+++ openstack-overcloud-deploy.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+openstack overcloud deploy --templates \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /home/stack/templates/scheduler_hints_env.yaml \
+-e /home/stack/templates/network-environment.yaml \
+-e /home/stack/templates/storage-environment.yaml \
+-e /home/stack/templates/rhel-registration/environment-rhel-registration.yaml \
+--control-flavor baremetal --compute-flavor baremetal --ceph-storage-flavor baremetal \
+--compute-scale 2 --control-scale 3  --ceph-storage-scale 3 \
+--libvirt-type kvm \
+--ntp-server 172.31.60.20 \
+--neutron-network-type vlan \
+--neutron-tunnel-type vlan \
+--neutron-bridge-mappings physnet-external:br-ex,physnet-tenant:br-bond0 \
+--neutron-network-vlan-ranges physnet-tenant:1000:1999 \
+--neutron-disable-tunneling --timeout 60 \
+--verbose --log-file overcloud_new.log 
+
+#--verbose --debug --log-file overcloud_new.log 
+
+#--rhel-reg --reg-method satellite --reg-org Default_Organization --reg-sat-url http://sat6.rolman.net --reg-activation-key osp8 \
+# DO NOT use https for Satellite registration
+#-e /home/stack/templates/ips-from-pool-all.yaml \
